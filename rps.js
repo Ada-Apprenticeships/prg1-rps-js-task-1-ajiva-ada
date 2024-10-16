c536c2e67d01809283e397d722a1bb3ca041f115
ajiva-ada <aaisha.jiva@ada.ac.uk>
2024-10-16 14:35:10 +0000

    adding shortcut

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
 function rockPaperScissors(player1, player2) {
   
-  const winningOptions = {
+  
+  const rules = {
+    
     rock: ['scissors', 'lizard'],
     paper: ['rock', 'spock'],
     scissors: ['paper', 'lizard'],
@@ -8,17 +10,24 @@ function rockPaperScissors(player1, player2) {
     lizard: ['spock', 'paper']
   };
   
+  
   if (player1 === player2) {
     return 'draw';
   }
+
+  return rules[player1].includes(player2) ? 'player1' : 'player2';
+}
   
-  if (winningOptions[player1].includes(player2)) {
+/* 
+Orginal code for identifying winning player
+  if (rules[player1].includes(player2)) {
     return 'player1';
   } else {
     return 'player2';
-  }
+  } 
+*/ 
 
-}
+  
 
 
 
